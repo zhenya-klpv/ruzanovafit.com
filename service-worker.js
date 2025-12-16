a73a61b10bd2d792c8495d9016a1959c5dd4fb1d
Zhenya Kolpakov <kolpakov.e@icloud.com>
2025-12-15 23:15:04 -0800

    About halo: match reference (neutral haze)

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 // Version 1.3.0
 
 // Bump cache name to force clients to refresh assets after deploys
-const CACHE_NAME = 'ruzanova-fitness-v21';
+const CACHE_NAME = 'ruzanova-fitness-v22';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
